e55c68a83a63d1a9d585e7ca3bd894d708fe6299
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-03-29 13:06:08 +0600

    PUT and DELETE functionality added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const shortid = require("shortid");
 const fs = require("fs/promises");
-const path = require('path')
+const path = require("path");
 
-const dbLocation = path.resolve('src', 'data.json')
+const dbLocation = path.resolve("src", "data.json");
 
 const app = express();
 app.use([morgan("dev"), cors(), express.json()]);
@@ -16,37 +16,99 @@ app.post("/", async (req, res) => {
     ...req.body,
   };
 
-  const data = await fs.readFile(dbLocation, 'utf8');
-  const players = JSON.parse(data)
-  players.push(player)
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
+  players.push(player);
 
-  await fs.writeFile(dbLocation, JSON.stringify(players))
+  await fs.writeFile(dbLocation, JSON.stringify(players));
 
   res.status(201).json(player);
 });
 
-app.get('/', async (req, res) => {
-  const data = await fs.readFile(dbLocation, 'utf8');
-  const players = JSON.parse(data)
+app.get("/", async (req, res) => {
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
 
-  res.status(200).json(players)
-})
+  res.status(200).json(players);
+});
 
-app.get('/:id', async (req, res) => {
-  const id = req.params.id
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
 
-  const data = await fs.readFile(dbLocation, 'utf8');
-  const players = JSON.parse(data)
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
 
-  const player = players.find(item => item.id === id)
-  console.log(player)
+  const player = players.find((item) => item.id === id);
+  console.log(player);
 
   if (!player) {
-    return res.status(404).json({ message: "Player Not Found!" })
+    return res.status(404).json({ message: "Player Not Found!" });
   }
 
-  res.status(200).json(player)
-})
+  res.status(200).json(player);
+});
+
+app.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
+
+  const player = players.find((item) => item.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: "Player Not Found!" });
+  }
+
+  player.name = req.body.name || player.name;
+  player.country = req.body.country || player.country;
+  player.rank = req.body.rank || player.rank;
+
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(200).json(player);
+});
+
+app.put("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
+
+  const player = players.find((item) => item.id === id);
+
+  if (!player) {
+    const player = {
+      id: shortid.generate(),
+      ...req.body,
+    };
+    players.push(player);
+  } else {
+    player.name = req.body.name;
+    player.country = req.body.country;
+    player.rank = req.body.rank;
+  }
+
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(200).json(player);
+});
+
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const data = await fs.readFile(dbLocation, "utf8");
+  const players = JSON.parse(data);
+
+  const player = players.find((item) => item.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: "Player Not Found!" });
+  }
+
+  const newPlayers = players.filter(item => item.id !== id)
+
+  await fs.writeFile(dbLocation, JSON.stringify(newPlayers));
+  res.status(203).send();
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Success" });
